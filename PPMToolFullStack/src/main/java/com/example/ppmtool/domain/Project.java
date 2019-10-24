807d683e4b35431f69b479d8192e72c536e05a5c
tgriggs603 <griggsy603@gmail.com>
2019-10-24 10:24:39 -0400

    added controller, validation service and custom exception. Adding services and exceptions packages

--- PPMToolFullStack/src/main/java/com/example/ppmtool/domain/Project.java
+++ PPMToolFullStack/src/main/java/com/example/ppmtool/domain/Project.java
@@ -1,6 +1,10 @@
 package com.example.ppmtool.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Entity
@@ -8,13 +12,22 @@ public class Project {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @NotBlank(message = "Project name is required")
     private String projectName;
+    @NotBlank(message = "Project identifier is required")
+    @Size(min=4, max=5, message = "Please use 4 to 5 characters")
+    @Column(updatable = false, unique = true)
     private String projectIdentifier;
-    private String Description;
+    @NotBlank(message = "Project description is required")
+    private String description;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date start_date;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date end_date;
 
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date created_At;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date updated_At;
 
     public Project(){
@@ -45,11 +58,11 @@ public class Project {
     }
 
     public String getDescription() {
-        return Description;
+        return description;
     }
 
-    public void setDescription(String description) {
-        Description = description;
+    public void setDescription(String descriptionSetter) {
+        description = descriptionSetter;
     }
 
     public Date getStart_date() {
